e7736045158f6b99db23d8240b28989a72ec6bd8
SLMCreatives <sulaiman@slmcreatives.com>
2025-09-27 23:38:04 +0800

    forgot password

--- sms-temp/app/auth/forgot-password/page.tsx
+++ sms-temp/app/auth/forgot-password/page.tsx
@@ -1,10 +1,13 @@
-import { ForgotPasswordForm } from "@/components/forgot-password-form";
+//import { ForgotPasswordForm } from "@/components/forgot-password-form";
 
 export default function Page() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <ForgotPasswordForm />
+        <p className="text-sm ">
+          If you forgor your password, please contact me via WhatsApp
+          (01121292383).
+        </p>
       </div>
     </div>
   );
