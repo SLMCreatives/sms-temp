01cd5ce51959cb8ab7064391203c8457d73185d8
SLMCreatives <sulaiman@slmcreatives.com>
2026-01-28 15:54:39 +0800

    sst page tracker

---
+++ sms-temp/app/sst/layout.tsx
@@ -0,0 +1,29 @@
+import { AuthButton } from "@/components/auth-button";
+import { NavMenuAuth } from "@/components/navigation";
+import { ThemeSwitcher } from "@/components/theme-switcher";
+
+export default function ProtectedLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <main className="min-h-screen flex flex-col items-center">
+      <div className="flex-1 w-full flex flex-col items-center">
+        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+            <NavMenuAuth />
+
+            <AuthButton />
+          </div>
+        </nav>
+        <div className="flex flex-col gap-20 w-full">{children}</div>
+
+        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+          <p>Powered by UNITAR Marketing Team ❤️</p>
+          <ThemeSwitcher />
+        </footer>
+      </div>
+    </main>
+  );
+}
